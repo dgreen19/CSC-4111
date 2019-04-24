7d30ae883abfded41ab5e1198c33a7531e63b9f4
shortforastormtrooper <greenjr91@gmail.com>
2019-04-23 20:39:14 -0400

    Commit for CR
    Commit for Change Request Project

--- Lab 6/UnitTestingTask/function.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
-using namespace std;
-int getFactorial(int val);
-int subArraySum(int a[], int n, int start);
-int check_anagram(char a[], char b[]);
-int getStrongNess(string& input);
-#endif
--- Lab 6/unittestdemo-master/calculator3/testheader.h
+++
@@ -1,5 +0,0 @@
-#ifndef DEMO_H
-#define DEMO_H
-using namespace std;
-int printSum(int val);
-#endif
